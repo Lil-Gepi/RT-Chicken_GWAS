3e1061905f60fcb7d1f6ed7ee1b62a05240fda54
yiwen <brandonchen2932@outlook.com>
2020-09-13 20:06:02 +0200

    the set up

---
+++ 01_QC.R
@@ -0,0 +1,8 @@
+##This is the QC procedures
+##1: Missingness of SNPs and individuals
+##2: Sex discrepancy
+##3: Minor allele frequency (MAF)
+##4: Hardy–Weinberg equilibrium (HWE)
+##5: Heterozygosity
+##6: Relatedness
+##(X)7: Population stratification
---
+++ 02_assc_test.R
@@ -0,0 +1,1 @@
+##This is the main procedure of the GWAS
