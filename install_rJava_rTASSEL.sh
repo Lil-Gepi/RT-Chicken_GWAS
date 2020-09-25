f6282e4b39cba5a2a5978a56d2db4d04e78290ae
Lil-Gepi <brandonchen2932@outlook.com>
2020-09-25 14:34:31 +0200

    script to install rJava and rTASSEL

---
+++ install_rJava_rTASSEL.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+apt update -y
+apt install -y openjdk-8-jdk openjdk-8-jre
+R CMD javareconf
+Rscript -e 'install.packages("rJava");library(rJava)'
+Rscript -e 'if (!require("devtools")) install.packages("devtools")'
+Rscript -e 'devtools::install_bitbucket( repo = "bucklerlab/rTASSEL", ref = "master", build_vignettes = FALSE)'
+
+
+
