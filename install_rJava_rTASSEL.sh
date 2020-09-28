867f6b4a42ea63eb12cb6964d9b0b6bc12e6f38d
Yiwen Chen <brandonchen2932@outlook.com>
2020-09-28 14:33:28 +0200

    Update install_rJava_rTASSEL.sh

--- install_rJava_rTASSEL.sh
+++ install_rJava_rTASSEL.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 apt update -y
 apt install -y openjdk-8-jdk openjdk-8-jre
-R CMD javareconf
+R CMD javareconf JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
 Rscript -e 'install.packages("rJava");library(rJava)'
 Rscript -e 'if (!require("devtools")) install.packages("devtools")'
 Rscript -e 'devtools::install_bitbucket( repo = "bucklerlab/rTASSEL", ref = "master", build_vignettes = FALSE)'
