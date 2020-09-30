5c71358a9873437055af1003171026e6ecc20148
Lil-Gepi <brandonchen2932@outlook.com>
2020-09-30 10:34:54 +0200

    added phenogeno

--- 01_ReadData.R
+++ 01_ReadData.R
@@ -4,20 +4,11 @@ library(rTASSEL)
 startLogger(fullPath = NULL, fileName = NULL)
 ##  read in the genotype data
 
-tasGenoDF <- rTASSEL::readGenotypeTableFromPath(path = "/home/yiwen/nas/all_gen2.vcf.gz")
-#tasGenoDF <- rTASSEL::readGenotypeTableFromPath(path = "/home/yiwen/nas/STITCH_imputed_genotypes_F1_to_F18/STITCH_NC_006115.5_Chr28_K10nGen19_SCOREHWE_no294BadSm.vcf.gz")
+tasGenoDF <- rTASSEL::readGenotypeTableFromPath(path = "/home/yiwen/RT-Chicken_GWAS/chr_all_gen2.vcf")
 tasGenoDF
 
 # Analyze the phenotype data
 pheno_csv <- read.csv("/home/yiwen/nas/AIL_phenotypes20190827.csv",header = T)
-
-#generations <- unique(pheno_csv$GENERATION)
-#genrations_samplesize <- data.frame()
-# for (i in 1:length(generations)) {
-#   genrations_samplesize[i,1] <- generations[i]
-#   genrations_samplesize[i,2] <- nrow(pheno_csv[pheno_csv$GENERATION == generations[i],])
-# }
-#so generation 15 is the biggest dataset
 
 pheno_gen2 <-pheno_csv[pheno_csv$GENERATION==2,] ; rm(pheno_csv)
 # remove the empty columns of the pheno data
@@ -60,18 +51,11 @@ tasPhenoDF <- rTASSEL::readPhenotypeFromDataFrame(
   taxaID = "Taxon",
   attributeTypes = phenoAttribute
 )
-
-# tasPhenoDF_tuto <- rTASSEL::readPhenotypeFromDataFrame(
-#   phenotypeDF = phenoDF_tuto,
-#   taxaID = "Taxon",
-#   attributeTypes = NULL
-# )
-# rm(tasPhenoDF_tuto)
-# Inspect new object
 tasPhenoDF
 
 tasGenoPheno <- rTASSEL::readGenotypePhenotype(
-  genoPathOrObj = tasGenoHMP,
-  phenoPathDFOrObj = tasPheno
+  genoPathOrObj = tasGenoDF,
+  phenoPathDFOrObj = tasPhenoDF
 )
+
 tasGenoPheno
