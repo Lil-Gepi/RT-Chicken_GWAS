7003820d5507e945ae950045899bf15cb1f99ab5
Lil-Gepi <brandonchen2932@outlook.com>
2020-09-30 13:52:39 +0200

    added comments

--- 01_ReadData.R
+++ 01_ReadData.R
@@ -2,48 +2,60 @@ options(java.parameters = c("-Xmx200g", "-Xms2g"))
 library(rJava)
 library(rTASSEL)
 startLogger(fullPath = NULL, fileName = NULL)
-##  read in the genotype data
-
+##------------------------------------------------------------------------------
+## Read in the genotype data
 tasGenoDF <- rTASSEL::readGenotypeTableFromPath(path = "/home/yiwen/RT-Chicken_GWAS/chr_all_gen2.vcf")
 tasGenoDF
 
-# Analyze the phenotype data
+##------------------------------------------------------------------------------
+## Read in the phenotype data
+
+# Read the original csv table
 pheno_csv <- read.csv("/home/yiwen/nas/AIL_phenotypes20190827.csv",header = T)
 
+# Only keep the generation 2 data
 pheno_gen2 <-pheno_csv[pheno_csv$GENERATION==2,] ; rm(pheno_csv)
-# remove the empty columns of the pheno data
-pheno_gen2_notnull <- Filter(function(x)!all(is.na(x) || is.null(x) || x == ""), pheno_gen2); rm(pheno_gen2)
-pheno_gen2_picked <- pheno_gen2_notnull[c("ID","BW8","GLUCOM","SEX")] ; row.names(pheno_gen2_picked) <- pheno_gen2_picked$ID
 
+# Remove the empty columns
+pheno_gen2_notnull <- Filter(function(x)!all(is.na(x) || is.null(x) || x == ""), pheno_gen2); rm(pheno_gen2)
+
+# Now we decide which trait are we interested in
+pheno_gen2_picked <- pheno_gen2_notnull[c("ID","BW8","GLUCOM","SEX")] ; row.names(pheno_gen2_picked) <- pheno_gen2_picked$ID ; rm(pheno_gen2_notnull)
+
+# Next we want to sort our pheno table by the same order of the sample id used in the geno vcf file
+# 1. we read the sample ids in vcf file with order.
 sample_w_geno <- read.csv(file = "/home/yiwen/RT-Chicken_GWAS/gen2_sample_id.txt",sep = "\n", header = F, col.names = "ID", stringsAsFactors = F); row.names(sample_w_geno) <- sample_w_geno$ID
+# 2. we order the pheno table accordingly
 pheno_ordered <- pheno_gen2_picked[row.names(sample_w_geno),]
-#checking how many of the samples don't have record in the traits
-sum(sapply(pheno_ordered$GLUCOM, function(x) is.na(x) || is.null(x) || x == ""))
-sum(sapply(pheno_ordered$BW8, function(x) is.na(x) || is.null(x) || x == ""))
-#If it's okay we can load the data into rTASSEL as following
-#phenoDF_tuto is an exampke from the tutorial, the phenoDF is the real one we gonna use
-phenoPath_tuto  <- system.file("extdata", "mdp_traits.txt", package = "rTASSEL")
-phenoDF_tuto <- read.table(phenoPath_tuto, header = TRUE) ; rm(phenoPath_tuto)
-colnames(phenoDF_tuto)[1] <- "Taxon"
-View(phenoDF_tuto)
-#so the first header should be "Taxon", with rest of them as is.
+
+# To tell rTassel what to expect, he first header should be "Taxon", with rest of them as is.
 phenoDF <- pheno_ordered; colnames(phenoDF)[1] <- "Taxon"
+
+# Following will be steps correct the type pf the data we have.
+# 1. we would prefer to have NAs instead of empty blocks in our pheno table.
 phenoDF[phenoDF==""]<-NA
-# However the GLUCOM data is actually characters and the decimal point is comma in Swedish way
-# We would like to change that to a normal numeric column.
+
+# 2. the GLUCOM data is actually character type and the decimal point is comma in Swedish way
+#    we would like to change that to a normal numeric column.
 phenoDF$GLUCOM <- as.character(phenoDF$GLUCOM)
 scan(text=phenoDF$GLUCOM, dec=",", sep=".")
 phenoDF$GLUCOM <- as.numeric(phenoDF$GLUCOM)
+
+# 3. tomake sure the other columns are also in the right type of our desire.
 phenoDF$SEX <- as.factor(phenoDF$SEX)
 phenoDF$Taxon <- as.character(phenoDF$Taxon)
+
+# 4. mic check mic check
 typeof(phenoDF$Taxon)
 typeof(phenoDF$BW8)
 typeof(phenoDF$GLUCOM)
 typeof(phenoDF$SEX)
 summary(phenoDF)
-####phenoDF <- phenoDF[!is.na(phenoDF$BW8), ]; phenoDF <- phenoDF[!is.na(phenoDF$GLUCOM), ]
-#execpt for the data itself, we also would like to specify what kind of data each column is,
-#so we'll need a vector to pass to attibuteTypes to do so.
+
+##[unnecessary]phenoDF <- phenoDF[!is.na(phenoDF$BW8), ]; phenoDF <- phenoDF[!is.na(phenoDF$GLUCOM), ]
+
+# Finally to read the phenotable into rTassel, execpt for the data itself, we also would like 
+# to specify what kind of data each column is, so we'll need a vector to pass to attibuteTypes to do so.
 phenoAttribute <- c( "data", "data", "factor")
 
 tasPhenoDF <- rTASSEL::readPhenotypeFromDataFrame(
@@ -51,13 +63,17 @@ tasPhenoDF <- rTASSEL::readPhenotypeFromDataFrame(
   taxaID = "Taxon",
   attributeTypes = phenoAttribute
 )
+# As we can see, this tasPhenoDF is in a strange format, yet we want to combine the genoDF with phenoDF now.
 tasPhenoDF
 
+# To combine, I chose to read from the two objects we have created, while the specification
+# used in creating tasPhenoDF needs to be passed to TASSEL again.
 tasGenoPheno <- rTASSEL::readGenotypePhenotype(
   genoPathOrObj = tasGenoDF,
   phenoPathDFOrObj = tasPhenoDF,
   taxaID = "Taxon",
   attributeTypes = NULL
 )
-
+# Let's take a look at this boiiiiii!
 tasGenoPheno
+save(tasGenoPheno, file = "/home/yiwen/RT-Chicken_GWAS/GenoPheno.RData")
